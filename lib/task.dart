88275dfe15e8369b9ad497705da437f674da893a
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-04-04 16:45:30 +0900

    add task model

---
+++ lib/task.dart
@@ -0,0 +1,8 @@
+class Task {
+  final String name;
+  final String memo;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Task(this.name, this.memo, this.createdAt, this.updatedAt);
+}
