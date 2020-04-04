0b5c885012456155cc3a92d6e7378b578a64ce2a
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-04-04 15:07:49 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/yamatatsutech/todo_app_yamatatsu/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yamatatsutech.todo_app_yamatatsu
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
