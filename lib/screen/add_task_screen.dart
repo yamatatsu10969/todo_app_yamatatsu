e738181780f342463ddbdb845c8d834d48bec388
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-04-04 15:35:17 +0900

    add navigator

--- lib/screen/add_task_screen.dart
+++ lib/screen/add_task_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatelessWidget {
+  static String id = 'add_task_screen';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Task'),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+      ),
     );
   }
 }
